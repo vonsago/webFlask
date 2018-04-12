5e1d6de88ec741c6a7dd16d46edcb808ef5d4372
vonsago <f811194414@gmail.com>
2018-04-12 14:25:39 +0800

    create web based flask

---
+++ admintest.sql
@@ -0,0 +1,42 @@
+﻿# MySQL-Front 5.1  (Build 4.13)
+
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE */;
+/*!40101 SET SQL_MODE='STRICT_TRANS_TABLES,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES */;
+/*!40103 SET SQL_NOTES='ON' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS */;
+/*!40014 SET FOREIGN_KEY_CHECKS=0 */;
+
+
+# Host: localhost    Database: admintest
+# ------------------------------------------------------
+# Server version 5.5.28
+
+#
+# Source for table user
+#
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+#
+# Dumping data for table user
+#
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'admin','admin');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
---
+++ flaskr.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+# coding=utf-8
+'''
+> File Name: flaskr.py
+> Author: vassago
+> Mail: f811194414@gmail.com
+> Created Time: 四  4/12 10:20:41 2018
+'''
+
+# all the imports
+import os
+import pymysql
+from flask import Flask, request, session, g, redirect, url_for, abort, \
+     render_template, flash
+
+app = Flask(__name__)
+app.secret_key = "super secret key"
+
+base_ip = '127.0.0.1'
+base_user = 'root'
+base_pwd = 'fengyufei123'
+base_db = 'admintest'
+
+
+def connect_db():
+    """Connects to the specific database."""
+    db = pymysql.connect(host = base_ip,user = base_user,charset = 'utf8',passwd = base_pwd,db = base_db)
+    return db
+
+def insert_db():
+    cur = connect_db()
+    cur.execute('')
+
+@app.route('/')
+def show_entries():
+    db = connect_db()
+    cur = db.cursor()
+    cur.execute('select name from user')
+    entry = [dict(title='第{}个用户'.format(str(row[0]+1)), text=row[1][0]) for row in enumerate(cur.fetchall())]
+    db.close()
+    return render_template('show_entries.html', entries=entry)
+
+@app.route('/add', methods=['POST'])
+def add_entry():
+    if not session.get('logged_in'):
+        abort(401)
+
+    flash('New entry was successfully posted')
+    return redirect(url_for('show_entries'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    db = connect_db()
+    cur = db.cursor()
+    cur.execute('select name,password from user')
+    pas = dict(cur.fetchall())
+    db.close()
+    if request.method == 'POST':
+        if pas.get(request.form['username']) == None :
+            error = 'Invalid username'
+        elif request.form['password'] != pas[request.form['username']]:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('show_entries'))
+    return render_template('login.html', error=error)
+
+@app.route('/signup', methods = ['GET','POST'])
+def signup():
+    error = None
+    sql = """INSERT INTO user(
+    name,password
+    )
+    VALUES (%s,%s)"""
+    if request.method == 'POST':
+        name = request.form['username']
+        pas  = request.form['password']
+        db = connect_db()
+        cur = db.cursor()
+        try:
+            cur.execute(sql,(name,pas))
+            db.commit()
+            db.close()
+            flash('New User successfully post')
+        except Exception as e:
+            error = e
+            flash('New User post error'+str(e))
+        return redirect(url_for('show_entries'))
+    return render_template('signup.html', error=error)
+
+
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('show_entries'))
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
+
