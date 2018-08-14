4192cc2407d2c25c26abf4c4d4beb593a4cdabf4
vonsago <f811194414@gmail.com>
2018-08-14 23:40:23 +0800

    storage

---
+++ app/unit_test/common.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding=utf-8
+'''
+> File Name: common.py
+> Author: vassago
+> Mail: f811194414@gmail.com
+> Created Time: 二  8/14 23:39:11 2018
+'''
+
+from unittest import TestCase
+
+class BaseTestCase(TestCase):
+    def create_test_app():
+        pass
+    pass
---
+++ app/unit_test/test_storage.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding=utf-8
+'''
+> File Name: test_storage.py
+> Author: vassago
+> Mail: f811194414@gmail.com
+> Created Time: 二  8/14 23:36:44 2018
+'''
+
+class 
+
+if __name__ == '__main__':
+    pass
